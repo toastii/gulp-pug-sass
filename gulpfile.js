3fccb7a9c03df40066f5a000b0f216550635633d
Florian Weigelt <florian@lancamp.de>
2018-11-12 09:43:54 +0100

    initial

---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+var
+  gulp = require('gulp'),
+  imagemin = require('gulp-imagemin'),
+  pug = require('gulp-pug'),
+  prefix = require('gulp-autoprefixer'),
+  sass = require('gulp-sass'),
+  browserSync = require('browser-sync'),
+  del = require('del'),
+
+  paths = {
+  src: 'src/**/*',
+  srcPUG: 'src/**/*.pug',
+  srcSASS: 'src/**/*.scss',
+  srcJS: 'src/**/*.js',
+  srcIMG: 'src/img/**/*',
+
+  dist: 'dist',
+  distIndex: 'dist/**/*.html',
+  distCSS: 'dist/**/*.css',
+  distJS: 'dist/**/*.js',
+  distIMG: 'dist/img/',
+};
+
+
+// pug
+
+gulp.task('pug', function () {
+  return gulp.src(paths.srcPUG)
+    .pipe(pug())
+    .on('error', function (err) {
+      process.stderr.write(err.message + '\n');
+      this.emit('end');
+    })
+    .pipe(gulp.dest(paths.dist))
+});
+
+
+// img
+
+gulp.task('img', function () {
+  return gulp.src(paths.srcIMG)
+    .pipe(imagemin({
+      optimizationLevel: 5
+    }))
+    .pipe(gulp.dest(paths.distIMG))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+
+// sass
+
+gulp.task('sass', function () {
+  return gulp.src(paths.srcSASS)
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+      cascade: true
+    }))
+    .pipe(gulp.dest(paths.dist))
+});
+
+
+// js
+
+gulp.task('js', function () {
+  gulp.src(paths.srcJS)
+    .pipe(gulp.dest(paths.distJS))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+// build
+
+gulp.task('build', ['clean', 'pug', 'sass', 'img', 'js'])
+
+
+// clean
+
+gulp.task('clean', function () {
+  console.log("Clean all files in dist");
+  return del('dist/*', {
+    force: true
+  });
+})
+
+
+// watch
+
+gulp.task('watch', ['sync'], function () {
+  gulp.watch(paths.src, ['build']);
+});
+
+gulp.task('default', ['build', 'watch']);
+
+
+// sync
+
+gulp.task('sync', function () {
+  browserSync({
+    server: {
+      baseDir: paths.dist
+    },
+    notify: false
+  });
+});
